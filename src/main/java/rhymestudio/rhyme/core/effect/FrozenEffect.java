7110a8c92530a7d28bb4a910d2c553c35a67c3ed
hhhyoy <3066740671@qq.com>
2025-01-31 18:22:53 +0800

    修改了碎冰逻辑，成了。

--- src/main/java/rhymestudio/rhyme/core/effect/FrozenEffect.java
+++ src/main/java/rhymestudio/rhyme/core/effect/FrozenEffect.java
@@ -8,35 +8,8 @@ import rhymestudio.rhyme.Rhyme;
 
 public class FrozenEffect extends MobEffect {
 
-//    private int count = -1;
-
     public FrozenEffect(MobEffectCategory category, int color) {
         super(category, color);
         addAttributeModifier(Attributes.MOVEMENT_SPEED, Rhyme.space("frozen"),-1.0, AttributeModifier.Operation.ADD_MULTIPLIED_BASE);
     }
-
-//    @Override
-//    public void onEffectStarted(LivingEntity livingEntity, int amplifier) {
-//        addAttributeModifier(Attributes.MOVEMENT_SPEED, Rhyme.space("frozen"),-1.0, AttributeModifier.Operation.ADD_MULTIPLIED_BASE);
-//        count++;
-//        System.out.println("onEffectStarted");
-//    }
-
-//    @Override
-//    public void onEffectAdded(LivingEntity livingEntity, int amplifier) {
-//        count = 0;
-//        System.out.println(livingEntity.getId() + ", " + "onEffectAdded: " + livingEntity.hasEffect(FROZEN_EFFECT) + "\tcount: " + count);
-//    }
-//
-//    @Override
-//    public void onMobHurt(LivingEntity livingEntity, int amplifier, DamageSource damageSource, float amount) {
-//        System.out.println("onMobHurt");
-//        if (count >= 2) {
-//            livingEntity.removeEffect(FROZEN_EFFECT);
-//            livingEntity.hurt(damageSource, amount);
-//            System.out.println(livingEntity.getId() + ", " + "double amount: " + livingEntity.hasEffect(FROZEN_EFFECT) + "\tcount: " + count);
-//        }
-//    }
-
-
 }
--- src/main/java/rhymestudio/rhyme/event/GameEvent.java
+++ src/main/java/rhymestudio/rhyme/event/GameEvent.java
@@ -1,45 +1,40 @@
 package rhymestudio.rhyme.event;
 
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.LivingEntity;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
 import net.neoforged.neoforge.event.server.ServerStartedEvent;
 import rhymestudio.rhyme.core.recipe.DaveTrades;
 
 import static rhymestudio.rhyme.Rhyme.MODID;
+import static rhymestudio.rhyme.core.registry.ModEffects.FROZEN_EFFECT;
 
 
 @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.GAME)
 public class GameEvent {
-
-//    static HashSet<Integer> frozenLivingEntity = new HashSet<Integer>();
 
     @SubscribeEvent
     public static void setUp(ServerStartedEvent event){
         DaveTrades.readTradesFromJson(event.getServer().getResourceManager());
     }
 
-//    @SubscribeEvent
-//    public static void onAttack(Pre event){
-//        LivingEntity entity = event.getEntity();
-//        int hashCode = entity.hashCode();
-//        if (entity.hasEffect(FROZEN_EFFECT)) {
-//            if (frozenLivingEntity.contains(hashCode)) {
-//                entity.removeEffect(FROZEN_EFFECT);
-//                entity.hurt(event.getSource(), event.getNewDamage());
-//                frozenLivingEntity.remove(hashCode);
-//            }
-//            else {
-//                frozenLivingEntity.add(hashCode);
-//            }
-//        }
-//    }
-
-//    @SubscribeEvent
-//    public static void onAttack(LivingDamageEvent.Pre event){
-//        LivingEntity entity = event.getEntity();
-//        if (entity.hasEffect(FROZEN_EFFECT)) {
-//            event.setNewDamage(event.getNewDamage() * 2);
-//            entity.removeEffect(FROZEN_EFFECT);
-//        }
-//    }
+    @SubscribeEvent
+    public static void onAttack(LivingDamageEvent.Pre event){
+        LivingEntity entity = event.getEntity();
+        if (entity.hasEffect(FROZEN_EFFECT)){
+            MobEffectInstance frozenEffectInstance = entity.getEffect(FROZEN_EFFECT);
+            if (frozenEffectInstance.getAmplifier() == 0){
+                int newAmplifier = 1;
+                int newDuration = frozenEffectInstance.getDuration(); // 保持原有持续时间
+                MobEffectInstance newEffectInstance = new MobEffectInstance(FROZEN_EFFECT, newDuration, newAmplifier);
+                entity.addEffect(newEffectInstance);
+            }
+            else{
+                event.setNewDamage(event.getNewDamage() * 2);
+                entity.removeEffect(FROZEN_EFFECT);
+            }
+        }
+    }
 }
